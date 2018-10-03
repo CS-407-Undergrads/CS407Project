3e0ce569911add106ac1bc06270a872220056d08
Ian McMahon <ianmcmahon1009@gmail.com>
2018-10-02 22:28:58 -0400

    full source file didn't upload

--- finishLater.js
+++ finishLater.js
@@ -29,4 +29,17 @@ var saveInfo = function() {
   }
 }
 
-var 
+//delete account information
+var deleteData = function(){
+  var answer;
+  //confirmation box to make sure user wants to delete
+  var response = confirm("Are you sure you want to delete?");
+  if(response == true){
+    answer = "deleted";
+    //setting savedData length to 0 erases contents of array
+    savedData.length = 0;
+  } else {
+    answer = "Account creation still in progress, nothing erased";
+  }
+
+}
