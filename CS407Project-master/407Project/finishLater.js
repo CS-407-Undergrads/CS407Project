cf2d0ea5438daab722dfc322fac678fe652a8594
Ian McMahon <ianmcmahon1009@gmail.com>
2018-10-03 22:40:49 -0400

    functions now fully operational

--- CS407Project-master/407Project/finishLater.js
+++ CS407Project-master/407Project/finishLater.js
@@ -4,35 +4,17 @@ var firstName, lastName, pwd, usrname;
 var saveInfo = function() {
 
 
-  // empty array
-  var savedData = [];
-  //assigning variables to elements with specific ID name, will change when
-  //proper ID name is declared
-  firstName = document.getElementsByName("first_name");
-  lastName = document.getElementsByName("last_name");
-  emailadd = document.getElementsByName("email");
-  usrname = document.getElementsByName("username");
-  //checks if field is null, if not pushs into array at the end
-  if(firstName != null){
-    savedData.push(firstName);
-  }
-  if(lastName != null){
-    savedData.push(lastName);
-  }
-  if(emailadd != null){
-    savedData.push(emailadd);
-  }
-  if(usrname != null){
-    savedData.push(usrname);
-  }
 
-  // find all elements with tag name input and assign them all to ""
-  var elements = document.getElementByTagName("input");
-  for(var i = 0; i <elements.length; i++){
-    if(elements[i].type == "text"){
-      elements[i].value = ""
-    }
-  }
+  var savedData = [];
+  //pushing input fields into js array
+  savedData.push(firstName = document.getElementsByName("first_name")[0].value);
+  savedData.push(lastName = document.getElementsByName("last_name")[0].value);
+  savedData.push(emailadd = document.getElementsByName("email")[0].value);
+  savedData.push(usrname = document.getElementsByName("username")[0].value);
+
+  savedData.toString();
+  //for testing
+  alert(savedData);
 }
 
 //delete account information
@@ -40,13 +22,16 @@ var deleteData = function(){
   var answer;
   //confirmation box to make sure user wants to delete
   var response = confirm("Are you sure you want to delete?");
+  // true = clicking ok/false vice versa
   if(response == true){
     answer = "deleted";
     alert(answer);
+    //reloads page to clear input
     location.reload();
 
     //setting savedData length to 0 erases contents of array
     savedData.length = 0;
+
   } else {
     answer = "Account creation still in progress, nothing erased";
     alert(answer);
