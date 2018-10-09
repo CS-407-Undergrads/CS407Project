8563019ce0138f300d759631e679972ac235e887
SoulConcerto <38120540+SoulConcerto@users.noreply.github.com>
2018-10-09 13:26:48 -0400

    Delete setsubendis.js

--- CS407Project-master/407Project/setsubendis.js
+++
@@ -1,5 +0,0 @@
-function checkTextFields() {
-        document.getElementById('submit').disabled = (name === "" || pass === "") ? true : false;
-}
-checkTextFields();
-setInterval(checkTextFields, 1);
